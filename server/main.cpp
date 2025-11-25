5321760324c41a38f1c556e26b5d342690c4a660
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-11-25 12:19:04 +0100

    🚧 Added comments for future implementation

--- server/main.cpp
+++ server/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-#include <cstdio>
 
 int main() {
-    printf("Server is running...\n");
+    // SPSC ring buffer queue initialization
+    // ASIO: TCP acceptor setup for client connections
+
+    // ASIO: UDP socket setup with handle packet function in a separate thread.
+    // It pushes events to the main thread via the SPSC queue and drops new
+    // events if it's full.
+
+    // R-Type: Event loop processing events from the queue and handling game
+    // logic, also handles sending UDP snapshots to clients.
+    std::cout << "Server is running..." << std::endl;
     return 0;
 }
