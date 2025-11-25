f1a40019239f002897c23b08162eab1ca1bfcd5f
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-11-25 16:19:47 +0100

    ✨ Add getter methods for Config class

--- server/include/server/Config.hpp
+++ server/include/server/Config.hpp
@@ -5,15 +5,34 @@
 namespace server {
 constexpr uint16_t DEFAULT_TCP_PORT = 50000;
 constexpr uint16_t DEFAULT_UDP_PORT = DEFAULT_TCP_PORT;
+constexpr std::string DEFAULT_TCP_ADDRESS = "0.0.0.0";
+constexpr std::string DEFAULT_UDP_ADDRESS = DEFAULT_TCP_ADDRESS;
 
 class Config {
  public:
     static Config &fromCommandLine(int argc, char *argv[]);
 
+    uint16_t getTcpPort() const {
+        return tcpPort;
+    }
+
+    uint16_t getUdpPort() const {
+        return udpPort;
+    }
+
+    const std::string &getTcpAddress() const {
+        return tcpAddress;
+    }
+
+    const std::string &getUdpAddress() const {
+        return udpAddress;
+    }
+
  private:
-    std::string tcpAddress = "0.0.0.0";
+    std::string tcpAddress = DEFAULT_TCP_ADDRESS;
     uint16_t tcpPort = DEFAULT_TCP_PORT;
-    std::string udpAddress = "0.0.0.0";   // Default is same as TCP address
+    std::string udpAddress =
+        DEFAULT_UDP_ADDRESS;              // Default is same as TCP address
     uint16_t udpPort = DEFAULT_UDP_PORT;  // Default is same as TCP port
 
     Config() = default;
