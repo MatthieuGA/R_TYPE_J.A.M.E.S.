8cfd958bc3cdc644f8f704f6b5f31e7e9e5348c1
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-11-25 14:19:14 +0100

    🚧 Update main function to accept command line arguments

--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
-int main() {
+#include <boost/asio.hpp>
+
+int main(int argc, char *argv[]) {
+    // Parse command line arguments for server configuration
+
     // SPSC ring buffer queue initialization
     // ASIO: TCP acceptor setup for client connections
 
@@ -9,7 +13,7 @@ int main() {
     // events if it's full.
 
     // R-Type: Event loop processing events from the queue and handling game
-    // logic, also handles sending UDP snapshots to clients.
+    // logic, also handles sending UDP snapshots to clients. Singleton class
     std::cout << "Server is running..." << std::endl;
     return 0;
 }
