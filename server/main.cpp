8fc74dfeedb06fe08296daca2e4647a6cc482959
MatthieuGA <matthieu.guillet-arnaud@epitech.eu>
2025-11-27 15:40:18 +0100

    :building_construction: Creation of the server class, with an ECS and the components describe in the RFC

--- server/main.cpp
+++ server/main.cpp
@@ -2,17 +2,16 @@
 
 #include <boost/asio.hpp>
 #include <server/Config.hpp>
-#include <server/Network.hpp>
+#include <server/Server.hpp>
 
 int main(int argc, char *argv[]) {
     try {
         boost::asio::io_context io;
         server::Config &cfg = server::Config::fromCommandLine(argc, argv);
-        server::Network network = server::Network(cfg, io);
-        // TODO(someone): Add Game class to manage game state and logic, give
-        // it a reference to Network so it can access the SPSC queue. Also
-        // give it the io_context so it can set timers for game ticks and
-        // use async_wait.
+        server::Server game_server(cfg, io);
+
+        game_server.initialize();
+        game_server.start();
 
         std::cout << "Server started, running io_context..." << std::endl;
         io.run();
