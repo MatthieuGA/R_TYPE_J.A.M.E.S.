b352711d64e6c2bc7daa29730a8b8eb6cda6fc15
Arthuryan Lohéac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-12-02 14:57:41 +0100

    🐛 [FIX] Corrected type from RigidBody to Velocity in positionSystem function

--- client/Engine/Systems/positionSystem.cpp
+++ client/Engine/Systems/positionSystem.cpp
@@ -9,7 +9,7 @@ namespace Com = Component;
 
 void positionSystem(Eng::registry &reg,
 Eng::sparse_array<Com::Transform> const &positions,
-Eng::sparse_array<Com::RigidBody> const &velocities) {
+Eng::sparse_array<Com::Velocity> const &velocities) {
     for (auto &&[i, pos, vel] :
         make_indexed_zipper(positions, velocities)) {
         //  std::cerr << "Entity " << i << " Position: ("
