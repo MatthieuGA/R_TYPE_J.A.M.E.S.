dc61e0fa7352b93dee290ebb60502fc947495067
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-12-03 17:40:32 +0100

    🐛 [Animation] Fixes potential divide by zero

--- client/Engine/Systems/animationSystem.cpp
+++ client/Engine/Systems/animationSystem.cpp
@@ -9,22 +9,27 @@ void NextFrame(Com::AnimatedSprite &anim_sprite, Com::Drawable &drawable) {
         else
             anim_sprite.currentFrame = anim_sprite.totalFrames - 1;
     }
+    if (drawable.texture.getSize().x == 0 || anim_sprite.frameWidth == 0)
+        return;
     const int columns = drawable.texture.getSize().x / anim_sprite.frameWidth;
-    const int left = (anim_sprite.currentFrame % columns)
-        * anim_sprite.frameWidth;
-    const int top = (anim_sprite.currentFrame / columns)
-        * anim_sprite.frameHeight;
-    drawable.sprite.setTextureRect(sf::IntRect(left, top,
-        anim_sprite.frameWidth, anim_sprite.frameHeight));
+    if (columns == 0)
+        return;
+    const int left =
+        (anim_sprite.currentFrame % columns) * anim_sprite.frameWidth;
+    const int top =
+        (anim_sprite.currentFrame / columns) * anim_sprite.frameHeight;
+    drawable.sprite.setTextureRect(sf::IntRect(
+        left, top, anim_sprite.frameWidth, anim_sprite.frameHeight));
     anim_sprite.elapsedTime = 0.0f;
 }
 
 void AnimationSystem(Eng::registry &reg, const sf::Clock &delta_time_clock,
-Eng::sparse_array<Com::AnimatedSprite> &anim_sprites,
-Eng::sparse_array<Com::Drawable> &drawables) {
+    Eng::sparse_array<Com::AnimatedSprite> &anim_sprites,
+    Eng::sparse_array<Com::Drawable> &drawables) {
     for (auto &&[i, anim_sprite, drawable] :
-    make_indexed_zipper(anim_sprites, drawables)) {
-        if (!drawable.isLoaded) continue;
+        make_indexed_zipper(anim_sprites, drawables)) {
+        if (!drawable.isLoaded)
+            continue;
         float dt = delta_time_clock.getElapsedTime().asSeconds();
         anim_sprite.elapsedTime += dt;
 
