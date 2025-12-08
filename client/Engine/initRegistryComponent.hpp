27f7675ce4fdc147d66f1ae141bea24bd048bdeb
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-12-08 18:07:57 +0100

    🚨 Add RenderComponent include to initRegistryComponents

--- client/Engine/initRegistryComponent.hpp
+++ client/Engine/initRegistryComponent.hpp
@@ -2,6 +2,7 @@
 #include "include/Components/CoreComponents.hpp"
 #include "include/Components/GameplayComponents.hpp"
 #include "include/Components/NetworkingComponents.hpp"
+#include "include/Components/RenderComponent.hpp"
 #include "include/Components/ScenesComponents.hpp"
 #include "include/registry.hpp"
 
