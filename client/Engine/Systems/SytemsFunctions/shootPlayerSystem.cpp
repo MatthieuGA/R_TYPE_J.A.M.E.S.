e88f0ecbdbd86359cc21123468a24f2af05860fa
Arthuryan Lohéac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-12-08 18:02:38 +0100

    🐛 Fix typos and improve constants in player and window configurations

--- client/Engine/Systems/SytemsFunctions/shootPlayerSystem.cpp
+++ client/Engine/Systems/SytemsFunctions/shootPlayerSystem.cpp
@@ -1,41 +1,44 @@
 #include <SFML/Graphics.hpp>
+
 #include "Engine/Systems/initRegistrySystems.hpp"
+#include "include/PlayerConst.hpp"
 
 namespace Rtype::Client {
 
 void createProjectile(Eng::registry &reg, float x, float y, int ownerId) {
     auto projectile_entity = reg.SpawnEntity();
-    const float speed = 800.0f;
     // Add components to projectile entity
     reg.AddComponent<Component::Transform>(projectile_entity,
         Component::Transform{x, y, 0.0f, 3.0f, Com::Transform::CENTER});
     reg.AddComponent<Component::Drawable>(projectile_entity,
         Component::Drawable("OriginalRtype/r-typesheet2.gif", -1));
-    reg.AddComponent<Component::AnimatedSprite>(projectile_entity,
-        Component::AnimatedSprite{24, 32, 10});
-    reg.AddComponent<Component::Projectile>(projectile_entity,
-        Component::Projectile{10.0f, speed, ownerId});
+    reg.AddComponent<Component::AnimatedSprite>(
+        projectile_entity, Component::AnimatedSprite{24, 32, 10});
+    reg.AddComponent<Component::Projectile>(
+        projectile_entity, Component::Projectile{PLAYER_DAMAGE_PROJECTILE,
+                               PLAYER_SPEED_PROJECTILE, ownerId});
 }
 
-void createChargedProjectile(Eng::registry &reg, float x, float y,
-int ownerId) {
+void createChargedProjectile(
+    Eng::registry &reg, float x, float y, int ownerId) {
     auto projectile_entity = reg.SpawnEntity();
-    const float speed = 600.0f;
     // Add components to projectile entity
     reg.AddComponent<Component::Transform>(projectile_entity,
         Component::Transform{x, y, 0.0f, 3.0f, Com::Transform::CENTER});
     reg.AddComponent<Component::Drawable>(projectile_entity,
         Component::Drawable("OriginalRtype/r-typesheet1.gif", -1));
-    reg.AddComponent<Component::AnimatedSprite>(projectile_entity,
-        Component::AnimatedSprite{29, 22, 25});
+    reg.AddComponent<Component::AnimatedSprite>(
+        projectile_entity, Component::AnimatedSprite{29, 22, 25});
     reg.AddComponent<Component::Projectile>(projectile_entity,
-        Component::Projectile{40.0f, speed, ownerId});
+        Component::Projectile{PLAYER_DAMAGE_CHARGED_PROJECTILE,
+            PLAYER_SPEED_CHARGED_PROJECTILE, ownerId});
 }
 
 /**
  * @brief System to handle player shooting mechanics.
  *
- * This system processes player inputs to manage shooting normal and charged projectiles.
+ * This system processes player inputs to manage shooting normal and charged
+ * projectiles.
  *
  * @param reg The registry containing components.
  * @param game_world The game world for accessing global state.
@@ -44,9 +47,9 @@ int ownerId) {
  * @param player_tags Sparse array of PlayerTag components.
  */
 void ShootPlayerSystem(Eng::registry &reg, GameWorld &game_world,
-Eng::sparse_array<Com::Transform> &transforms,
-Eng::sparse_array<Com::Inputs> const &inputs,
-Eng::sparse_array<Com::PlayerTag> &player_tags) {
+    Eng::sparse_array<Com::Transform> &transforms,
+    Eng::sparse_array<Com::Inputs> const &inputs,
+    Eng::sparse_array<Com::PlayerTag> &player_tags) {
     for (auto &&[i, transform, input, player_tag] :
         make_indexed_zipper(transforms, inputs, player_tags)) {
         // Handle shooting normal projectiles
--- client/Engine/gameWorld.hpp
+++ client/Engine/gameWorld.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+#include "include/WindowConst.hpp"
 #include "include/registry.hpp"
 
 #include "Engine/Events/Event.h"
@@ -16,11 +17,11 @@ struct GameWorld {
     EventBus event_bus_;
 
     GameWorld()
-        : window_(sf::VideoMode({1920, 1080}), "Rtype") {
+        : window_(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), WINDOW_TITLE) {
         registry_ = Engine::registry();
         delta_time_clock_ = sf::Clock();
         event_bus_ = EventBus();
-        window_size_ = sf::Vector2f({1920.f, 1080.f});
+        window_size_ = sf::Vector2f({WINDOW_WIDTH, WINDOW_HEIGHT});
     }
 };
 }  // namespace Rtype::Client
