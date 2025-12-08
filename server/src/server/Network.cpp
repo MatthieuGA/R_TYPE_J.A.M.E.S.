70d9f415867a0df74b6e9b96114c9870fcd8853f
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-12-08 16:21:24 +0100

    ✨ Enhance TCP connection handling with callback support in Network class

--- server/src/server/Network.cpp
+++ server/src/server/Network.cpp
@@ -15,14 +15,14 @@ Network::Network(Config &config, boost::asio::io_context &io)
 }
 
 Network::UDP::UDP(Config &config, boost::asio::io_context &io)
-: socket(io, boost::asio::ip::udp::endpoint(
-                        boost::asio::ip::make_address(config.getUdpAddress()),
-                        config.getUdpPort())) {}
+    : socket(io, boost::asio::ip::udp::endpoint(
+                     boost::asio::ip::make_address(config.getUdpAddress()),
+                     config.getUdpPort())) {}
 
 Network::TCP::TCP(Config &config, boost::asio::io_context &io)
-: acceptor(io, boost::asio::ip::tcp::endpoint(
-                        boost::asio::ip::make_address(config.getTcpAddress()),
-                        config.getTcpPort())) {}
+    : acceptor(io, boost::asio::ip::tcp::endpoint(
+                       boost::asio::ip::make_address(config.getTcpAddress()),
+                       config.getTcpPort())) {}
 
 boost::asio::ip::port_type Network::UDP::port() const {
     return socket.local_endpoint().port();
@@ -39,9 +39,16 @@ void Network::TCP::accept() {
             if (!ec) {
                 std::cout << "New TCP connection from "
                           << socket.remote_endpoint().address().to_string()
-                          << ":" << port() << std::endl;
-                sockets.push_back(std::move(socket));
-                // Async receive and send will be handled elsewhere
+                          << ":" << socket.remote_endpoint().port()
+                          << std::endl;
+
+                // Transfer socket ownership to Server class via callback
+                if (on_accept_) {
+                    on_accept_(std::move(socket));
+                } else {
+                    std::cerr << "Warning: No TCP accept callback registered, "
+                              << "closing connection" << std::endl;
+                }
             } else {
                 std::cerr << "Error accepting TCP connection: " << ec.message()
                           << std::endl;
