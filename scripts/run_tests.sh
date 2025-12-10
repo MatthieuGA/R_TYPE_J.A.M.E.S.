1c150b8c53c93f58149cc4fce34908b4f31d2a5b
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-12-10 11:01:14 +0100

    🔨 [Tests] Add script to run engine and server tests
    #129

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,15 @@
 cd build
+
+echo -e "\n######################################################################"
+echo -e "🚀 Running Engine Tests..."
+echo -e "######################################################################\n"
+
 cmake --build . --target engine_tests
 ./tests/engine_tests
+
+echo -e "\n######################################################################"
+echo -e "🚀 Running Server Tests..."
+echo -e "######################################################################\n"
+
+cmake --build . --target server_tests
+./tests/server_tests
