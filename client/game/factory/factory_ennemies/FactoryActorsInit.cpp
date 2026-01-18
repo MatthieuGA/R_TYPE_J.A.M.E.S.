5f4ae6bf5e878e9088fedf5fb8a3204eef9ab923
Samuel Giret <samuel.giret@epitech.eu>
2026-01-18 01:07:15 +0100

    ✨ Implement boss enemy mechanics (no more frame updating) and adjust projectile speeds

--- client/game/factory/factory_ennemies/FactoryActorsInit.cpp
+++ client/game/factory/factory_ennemies/FactoryActorsInit.cpp
@@ -29,7 +29,7 @@ std::string GetStringFromJson(const nlohmann::json &j, const std::string &key,
 
 float GetFloatFromJson(
     const nlohmann::json &j, const std::string &key, float defaultValue) {
-    if (j.contains(key) && j[key].is_number_float())
+    if (j.contains(key) && j[key].is_number())
         return j[key].get<float>();
     return defaultValue;
 }
@@ -41,9 +41,9 @@ Engine::Graphics::Vector2f GetVector2fFromJson(const nlohmann::json &j,
     if (j.contains(key) && j[key].is_object()) {
         float x = defaultValue.x;
         float y = defaultValue.y;
-        if (j[key].contains("x") && j[key]["x"].is_number_float())
+        if (j[key].contains("x") && j[key]["x"].is_number())
             x = j[key]["x"].get<float>();
-        if (j[key].contains("y") && j[key]["y"].is_number_float())
+        if (j[key].contains("y") && j[key]["y"].is_number())
             y = j[key]["y"].get<float>();
         return Engine::Graphics::Vector2f(x, y);
     }
--- client/include/PlayerConst.hpp
+++ client/include/PlayerConst.hpp
@@ -7,15 +7,15 @@ constexpr const float PLAYER_SHOOT_COOLDOWN = 0.2f;
 constexpr const float PLAYER_CHARGE_TIME = 0.5f;
 constexpr const int PLAYER_INITIAL_HEALTH = 3;
 
-constexpr const float PLAYER_SPEED_PROJECTILE = 800.0f;
+constexpr const float PLAYER_SPEED_PROJECTILE = 500.0f;
 constexpr const int PLAYER_DAMAGE_PROJECTILE = 10;
 
-constexpr const float PLAYER_SPEED_CHARGED_PROJECTILE = 500.0f;
-constexpr const int PLAYER_DAMAGE_CHARGED_PROJECTILE = 50;
+constexpr const float PLAYER_SPEED_CHARGED_PROJECTILE = 250.0f;
+constexpr const int PLAYER_DAMAGE_CHARGED_PROJECTILE = 500;
 
 constexpr const float PLAYER_GATLING_DURATION = 5.0f;
 constexpr const float PLAYER_GATLING_SHOOT_INTERVAL = 0.1f;
 constexpr const int PLAYER_GATLING_DAMAGE = 5;
-constexpr const float PLAYER_GATLING_PROJECTILE_SPEED = 1500.0f;
+constexpr const float PLAYER_GATLING_PROJECTILE_SPEED = 750.0f;
 
 }  // namespace Rtype::Client
